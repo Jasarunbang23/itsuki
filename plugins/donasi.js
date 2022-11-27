0443b936db85336a383d8b2c1fb78222cc8e1def
Jasarunbang23 <119160730+Jasarunbang23@users.noreply.github.com>
2022-11-27 17:16:06 +0700

    Update donasi.js

--- plugins/donasi.js
+++ plugins/donasi.js
@@ -2,15 +2,9 @@ let handler =  m => m.reply(`
 ╭─「 🧚🏻‍♂️ *DONASI* 」
 │
 ├ PULSA / DANA /  :
-├ • *083137354143*
+├ • *085710264622*
 │
-├ • SAWERIA
-├ No Link Saweria
-│
-├ • OWNERKU
-├ wa.me/62895330014743
 ╰───────────
-© 𝙋𝙤𝙬𝙚𝙧𝙚𝙙 𝘽𝙮 𝐅𝐢𝐤𝐫𝐢𝐢
 `.trim()) // Tambah sendiri kalo mau
 
 handler.menugroup = ['donasi']
